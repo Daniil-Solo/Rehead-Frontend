68b36f56ebf78b53a648b324a5c9462d5af7bcf6
Daniil-Solo <daniil.solo1723@gmail.com>
2023-09-04 10:42:43 +0500

    reset styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import { AuthPage } from './pages/auth/auth-page';
 
 function App() {
   return (
     <div className="App">
-     
+      <AuthPage/>
     </div>
   );
 }
