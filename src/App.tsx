d0d50680cbe2c99675c8ac5403dba8172ede41eb
Daniil-Solo <daniil.solo1723@gmail.com>
2023-09-04 17:49:32 +0500

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import {Routes, Route} from 'react-router-dom'
 import './App.css';
-import { AuthPage } from './pages/auth/auth-page';
+import { AuthPage } from './pages/auth-page/auth-page';
+import { LoginForm } from './pages/auth-page/auth-form/login-form';
+import { RegisterForm } from './pages/auth-page/auth-form/register-form';
+import { PageNotFound } from './pages/page-not-found/page-not-found';
 
 function App() {
   return (
-    <div className="App">
-      <AuthPage/>
-    </div>
+    <Routes>
+      <Route path="/login" element={<AuthPage> <LoginForm/> </AuthPage>} />
+      <Route path="/register" element={<AuthPage> <RegisterForm/> </AuthPage>} />
+      <Route path="*" element={<PageNotFound/>} />
+    </Routes>
   );
 }
 
