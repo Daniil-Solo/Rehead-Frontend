0a4ecac9e1f87a1821812634f8c649f1c713ad3c
Daniil-Solo <daniil.solo1723@gmail.com>
2023-09-06 14:48:01 +0500

    add register api

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,4 +1,4 @@
-import {AxiosError, isAxiosError} from 'axios';
+import {isAxiosError} from 'axios';
 import axios from "./axios";
 
 const LOGIN_URL = "/users/auth/login";
@@ -24,7 +24,6 @@ export const runLogin = async (login: string, password: string): Promise<string>
         const accesToken = response.data.access_token;
         return accesToken;
     } catch (err){
-        console.log(err)
         let message = "";
         if (!isAxiosError(err)) {
             message = "Нет ответа от сервера";
@@ -33,6 +32,42 @@ export const runLogin = async (login: string, password: string): Promise<string>
                 message = "Неправильный логин или пароль"
             } else {
                 message = "Пользователь заблокирован"
+            }
+        } else if (err.response?.status === 422){
+            message = "Одно из полей не заполнено";
+        } else {
+            message = "Возникла ошибка на сервере";
+        }
+        throw Error(message);
+    }
+}
+
+export const runRegister = async (login: string, password: string): Promise<string> => {
+    try {
+        await axios.post(
+            REGISTER_URL,
+            {
+                email: login,
+                password: password
+            },
+            {
+                headers: { 
+                    'Content-Type': 'application/json' 
+                },
+                withCredentials: true
+            }
+        )
+        const accesToken = await runLogin(login, password);
+        return accesToken;
+    } catch (err){
+        let message = "";
+        if (!isAxiosError(err)) {
+            message = "Нет ответа от сервера";
+        } else if (err.response?.status === 400){
+            if (err.response.data.detail === "REGISTER_USER_ALREADY_EXISTS"){
+                message = "Пользователь с таким email уже существует"
+            } else {
+                message = "Слишком простой пароль"
             }
         } else if (err.response?.status === 422){
             message = "Одно из полей не заполнено";
