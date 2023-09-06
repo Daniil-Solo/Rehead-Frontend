7b3f3ed844f88e27ba47bd7c442141fe86614a25
Daniil-Solo <daniil.solo1723@gmail.com>
2023-09-06 19:15:15 +0500

    move big-logo to public

--- src/pages/auth-page/auth-page.tsx
+++ src/pages/auth-page/auth-page.tsx
@@ -8,7 +8,7 @@ interface AuxProps {
 export const AuthPage: React.FC<AuxProps> = ({children}) => {
     return (
         <div className="auth-page">
-            <div className="auth-page__big_column">
+            <div className="auth-page__big_column" style={{ backgroundImage: `url(images/big-logo.jpg)`}}>
             </div>
             <div className="auth-page__small_column auth-background">
                 {children}
