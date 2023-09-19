c18a416b9231ac6cbe3134a15ff58f6be68c59f8
Daniil-Solo <daniil.solo1723@gmail.com>
2023-09-20 00:04:43 +0500

    add dotenv

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,3 +1,14 @@
 import axios from "axios";
 
-export default axios.create();
+
+const nodeEnv = process.env.NODE_ENV;
+let baseURL = undefined;
+if (nodeEnv === 'development')
+    baseURL = process.env.REACT_APP_DEVELOP_API_URL;
+else if (nodeEnv === 'test')
+    baseURL = process.env.REACT_APP_TEST_API_URL;
+
+
+export default axios.create({
+    baseURL: baseURL
+});
